e040e51c0579a93976a68cb0c4c080c77c8609ac
PrinceLumpy <Princelumpypackmule@gmail.com>
2026-09-25 17:21:59 -0400

    Fix 16kb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,9 @@ android {
     }
 }
 
+configurations.configureEach {
+    exclude(group = "androidx.graphics", module = "graphics-path")
+}
 
 
 dependencies {
@@ -65,7 +68,9 @@ dependencies {
     // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.graphics) {
+        exclude(group = "androidx.graphics", module = "graphics-path")
+    }
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
@@ -76,7 +81,6 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.foundation.layout)
     implementation(libs.androidx.material3)
     ksp(libs.hilt.compiler)
